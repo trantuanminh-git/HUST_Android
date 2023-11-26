1225d1726520f5ca7c59955dbc168700282e785c
trantuanminh-git <trantuanminh9802@gmail.com>
2023-11-26 09:09:23 +0700

    getter and setter

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -3,19 +3,26 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
     init {
         println("aquarium initializing")
     }
-    init {
-        // 1 liter = 1000 cm^3
-        println("Volume: ${width * length * height / 1000} liters")
-    }
+//    init {
+//        // 1 liter = 1000 cm^3
+//        println("Volume: ${width * length * height / 1000} liters")
+//    }
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
         val tank = numberOfFish * 2000 * 1.1
         // calculate the height needed
         height = (tank / (length * width)).toInt()
     }
+
+    var volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
+        set(value) {
+            height = (value*1000) / (width*length)
+        }
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        println("Volume in printSize: $volume liters")
     }
 }
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,20 +1,22 @@
 package example.myapp
 
 fun buildAquarium() {
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    // default height and length
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    // default width
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    // everything custom
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
+//    val aquarium1 = Aquarium()
+//    aquarium1.printSize()
+//    // default height and length
+//    val aquarium2 = Aquarium(width = 25)
+//    aquarium2.printSize()
+//    // default width
+//    val aquarium3 = Aquarium(height = 35, length = 110)
+//    aquarium3.printSize()
+//    // everything custom
+//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+//    aquarium4.printSize()
     val aquarium6 = Aquarium(numberOfFish = 29)
     aquarium6.printSize()
-    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
+//    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
+    aquarium6.volume = 70
+    aquarium6.printSize()
 }
 
 fun main() {
