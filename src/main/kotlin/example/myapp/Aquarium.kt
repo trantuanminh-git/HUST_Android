62e999611fcb6ad36f9d21ba191e02ec9d2822ce
trantuanminh-git <trantuanminh9802@gmail.com>
2024-01-02 19:20:16 +0700

    subclass and inheritance

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,9 +1,11 @@
 package example.myapp
-class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import java.lang.Math.PI
+open class Aquarium (open var length: Int = 25, open var width: Int = 25, open var height: Int = 40) {
     init {
         println("aquarium initializing")
     }
-//    init {
+
+    //    init {
 //        // 1 liter = 1000 cm^3
 //        println("Volume: ${width * length * height / 1000} liters")
 //    }
@@ -14,15 +16,32 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
         height = (tank / (length * width)).toInt()
     }
 
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
         set(value) {
-            height = (value*1000) / (width*length)
+            height = (value * 1000) / (width * length)
         }
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
-        println("Volume in printSize: $volume liters")
+        // 1l = 1000cm^3
+        println("Volume in printSize: $volume liters, water: $water liters, (${water / volume * 100}% full)")
     }
+}
+
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width / 2 * length / 2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width / 2 * length / 2)).toInt()
+        }
+    override var water = volume * 0.8
+    override val shape = "cylinder"
 }
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,24 +1,19 @@
 package example.myapp
 
 fun buildAquarium() {
-//    val aquarium1 = Aquarium()
-//    aquarium1.printSize()
-//    // default height and length
-//    val aquarium2 = Aquarium(width = 25)
-//    aquarium2.printSize()
-//    // default width
-//    val aquarium3 = Aquarium(height = 35, length = 110)
-//    aquarium3.printSize()
-//    // everything custom
-//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-//    aquarium4.printSize()
-    val aquarium6 = Aquarium(numberOfFish = 29)
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
     aquarium6.printSize()
-//    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
+
+//fun testMySecondWithContext() {
+//    GlobalScope.lanch(Dispatcher.IO) {
+//
+//    }
+//}
 
 fun main() {
     buildAquarium()
+//    val job1: Job =
 }
