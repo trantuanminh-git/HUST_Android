9feee8e72d5fd0b2a0dfb380ca12e5549d95f9cc
trantuanminh-git <trantuanminh9802@gmail.com>
2024-01-07 18:24:04 +0700

    interface delegation

--- src/main/kotlin/example/myapp/AquariumFish.kt
+++ src/main/kotlin/example/myapp/AquariumFish.kt
@@ -1,21 +1,42 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
+//abstract class AquariumFish {
+//    abstract val color: String
+//}
+interface FishColor {
+    val color: String
 }
 interface FishAction  {
     fun eat()
 }
-class Shark: AquariumFish(), FishAction {
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+class PrintFishAction(val food: String): FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+class Shark: FishColor, FishAction {
     override val color = "grey"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction {
-    override val color = "gold"
-    override fun eat() {
-        println("eat algae")
-    }
-}
+//class Plecostomus: FishColor by GoldColor, FishAction {
+//    override fun eat() {
+//        println("eat algae")
+//    }
+//}
+// -----------------------
+//class Plecostomus(fishColor: FishColor = GoldColor):  FishAction,
+//        FishColor by fishColor {
+//    override fun eat() {
+//        println("eat algae")
+//    }
+//}
+// -----------------------
+class Plecostomus(fishColor: FishColor = GoldColor):
+        FishAction by PrintFishAction("eat algae"),
+        FishColor by GoldColor
